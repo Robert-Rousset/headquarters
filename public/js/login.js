c7a46b5fb9f7cd5c3313ae8c30f3e516213b1e05
Shelbyrp <77222563+Shelbyrp@user.noreply.github.com>
2021-06-13 17:22:21 +0800

    Added a signup modal.

--- public/js/login.js
+++ public/js/login.js
@@ -1,4 +1,4 @@
-async function toggleModal(event){
+async function toggleLoginModal(event){
     event.preventDefault();
 
     const modal = document.querySelector('#login-modal')
@@ -6,6 +6,18 @@ async function toggleModal(event){
     modal.classList.toggle("is-active");
 }
 
-document.querySelector('#login').addEventListener('click', toggleModal);
-document.querySelector('.modal-background').addEventListener('click', toggleModal);
-document.querySelector('.delete').addEventListener('click', toggleModal);
+async function toggleSignupModal(event){
+    event.preventDefault();
+
+    const modal = document.querySelector('#signup-modal')
+    // modal.setAttribute("class", "is-active");
+    modal.classList.toggle("is-active");
+}
+
+document.querySelector('#signup').addEventListener('click', toggleSignupModal);
+document.querySelector('#signup-background').addEventListener('click', toggleSignupModal);
+document.querySelector('#signup-delete').addEventListener('click', toggleSignupModal);
+
+document.querySelector('#login').addEventListener('click', toggleLoginModal);
+document.querySelector('#login-background').addEventListener('click', toggleLoginModal);
+document.querySelector('#login-delete').addEventListener('click', toggleLoginModal);
