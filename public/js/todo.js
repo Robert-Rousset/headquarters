a57c4b502e0c6299109005da5adbba9c73848ee7
John Barker <jdabarker@gmail.com>
2021-06-15 18:50:51 +0800

    error handling try/catch

--- public/js/todo.js
+++ public/js/todo.js
@@ -1,99 +1,121 @@
 let selectedColour;
 let selectedTodoId;
 
-async function toggleTodoModal(event) {
-    event.preventDefault();
+function toggleTodoModal(event) {
+  event.preventDefault();
 
-    const clickedOnAnEdit = Array.from(event.currentTarget.classList).includes("edit")
-    if (clickedOnAnEdit) {
-        document.querySelector('#create-todo-confirm').style.display = "none";
-        document.querySelector('#update-todo-confirm').style.display = "flex";
-        document.querySelector("#todo-title").value = this.dataset.todoTitle;
-        selectedTodoId = this.dataset.todoId;
-    } else {
-        document.querySelector('#create-todo-confirm').style.display = "flex";
-        document.querySelector('#update-todo-confirm').style.display = "none";
-        document.querySelector("#todo-title").value = "";
-    }
+  const clickedOnAnEdit = Array.from(event.currentTarget.classList).includes(
+    "edit"
+  );
+  if (clickedOnAnEdit) {
+    document.querySelector("#create-todo-confirm").style.display = "none";
+    document.querySelector("#update-todo-confirm").style.display = "flex";
+    document.querySelector("#todo-title").value = this.dataset.todoTitle;
+    selectedTodoId = this.dataset.todoId;
+  } else {
+    document.querySelector("#create-todo-confirm").style.display = "flex";
+    document.querySelector("#update-todo-confirm").style.display = "none";
+    document.querySelector("#todo-title").value = "";
+  }
 
-    const modal = document.querySelector("#todo-modal");
-    modal.classList.toggle("is-active");
+  const modal = document.querySelector("#todo-modal");
+  modal.classList.toggle("is-active");
 }
 
 async function createList(event) {
+  try {
     event.preventDefault();
 
     const title = document.querySelector("#todo-title").value.trim();
     const response = await fetch("/api/users/create-todo", {
-        method: "POST",
-        body: JSON.stringify({
-            title,
-            selectedColour,
-        }),
-        headers: {
-            "Content-Type": "application/json",
-        },
+      method: "POST",
+      body: JSON.stringify({
+        title,
+        selectedColour,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
     });
     if (response.ok) {
-        document.location.replace("/todo");
+      document.location.replace("/todo");
     }
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 async function updateList(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const title = document.querySelector("#todo-title").value.trim();
-    const response = await fetch(`/api/users/update-todo/${selectedTodoId}`, {
-        method: "PUT",
-        body: JSON.stringify({
-            title,
-            selectedColour,
-        }),
-        headers: {
-            "Content-Type": "application/json",
-        },
-    });
-    if (response.ok) {
-        document.location.replace("/todo");
-    }
+  const title = document.querySelector("#todo-title").value.trim();
+  const response = await fetch(`/api/users/update-todo/${selectedTodoId}`, {
+    method: "PUT",
+    body: JSON.stringify({
+      title,
+      selectedColour,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  if (response.ok) {
+    document.location.replace("/todo");
+  }
 }
 
-const colours = Array.from(document.querySelectorAll(".colour"));
-colours.forEach((colour) => {
+function setUpColors() {
+  const colours = Array.from(document.querySelectorAll(".colour"));
+  colours.forEach((colour) => {
     colour.addEventListener("click", function selectColour(event) {
-        event.preventDefault();
+      event.preventDefault();
 
-        const coloursEls = Array.from(document.querySelectorAll(".colour"));
-        coloursEls.forEach((element) => {
-            element.classList.remove("selected");
-        });
+      const coloursEls = Array.from(document.querySelectorAll(".colour"));
+      coloursEls.forEach((element) => {
+        element.classList.remove("selected");
+      });
 
-        this.classList.add("selected");
+      this.classList.add("selected");
 
-        const classesArray = Array.from(this.classList);
+      const classesArray = Array.from(this.classList);
 
-        selectedColour = classesArray.find((element) => element.startsWith("is-"));
+      selectedColour = classesArray.find((element) =>
+        element.startsWith("is-")
+      );
     });
-});
+  });
+}
 
-const edits = Array.from(document.querySelectorAll(".edit"));
-edits.forEach((edit) => {
-    edit.addEventListener("click", toggleTodoModal)
-});
+function setUpEdits() {
+  const edits = Array.from(document.querySelectorAll(".edit"));
+  edits.forEach((edit) => {
+    edit.addEventListener("click", toggleTodoModal);
+  });
+}
 
-document
+function setUpModal() {
+  document
     .querySelector("#todo-background")
     .addEventListener("click", toggleTodoModal);
-document
+  document
     .querySelector("#todo-delete")
     .addEventListener("click", toggleTodoModal);
-document
+  document
     .querySelector("#new-list")
     .addEventListener("click", toggleTodoModal);
-document
+  document
     .querySelector("#create-todo-confirm")
     .addEventListener("click", createList);
 
-document
+  document
     .querySelector("#update-todo-confirm")
     .addEventListener("click", updateList);
+}
+
+function init() {
+  setUpColors();
+  setUpEdits();
+  setUpModal();
+}
+
+init();
