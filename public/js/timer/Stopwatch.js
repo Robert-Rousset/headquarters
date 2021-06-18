6fcae1fa51854b28ee9d0694005bedf6ae8b93ca
Shelbyrp <77222563+Shelbyrp@user.noreply.github.com>
2021-06-18 15:50:12 +0800

    Create timer.

---
+++ public/js/timer/Stopwatch.js
@@ -0,0 +1,25 @@
+export default class {
+    constructor(startSeconds) {
+        this.seconds = startSeconds;
+        console.log(this.seconds);
+    }
+    start() {
+        if (this.seconds <= 0) {
+            return;
+        }
+        this.intervalId = setInterval(this.tick, 1000, this);
+    }
+    tick(timer) {
+        timer.seconds -= 1;
+        if (timer.seconds <= 0) {
+            timer.complete();
+        }
+    }
+    pause() {
+        clearInterval(this.intervalId);
+    }
+    complete() {
+        clearInterval(this.intervalId);
+        alert("Your time is up!");
+    }
+}
---
+++ public/js/timer/timer.js
@@ -0,0 +1,36 @@
+import Stopwatch from "./Stopwatch.js";
+
+function init() {
+    initTimerSettingsButton();
+    initTimerSettingsConfirmButton();
+    const stopwatch = new Stopwatch(3);
+    stopwatch.start();
+}
+
+function initTimerSettingsButton() {
+    const timerSettingsButton = document.querySelector("#timer-settings-button");
+    timerSettingsButton.addEventListener("click", editTimer)
+}
+
+function editTimer(_event) {
+    showTimerSettingsModal()
+}
+
+function showTimerSettingsModal() {
+    const timerSettingsModal = document.querySelector("#timer-settings-modal");
+    timerSettingsModal.setAttribute("class", "modal is-active")
+}
+
+function initTimerSettingsConfirmButton() {
+    const timerSettingsConfirmButton = document.querySelector("#timer-settings-confirm-button")
+    timerSettingsConfirmButton.addEventListener("click", confirmTimerSettings)
+}
+
+function confirmTimerSettings() {
+    const selectedTimeString = document.querySelector("select").value;
+    const arrayOfStrings = selectedTimeString.split(" ")
+    const amount = Number(arrayOfStrings[0])
+    const units = arrayOfStrings[1]
+}
+
+init();
